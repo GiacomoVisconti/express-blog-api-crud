ff89e64f44779a7552b6066a832bd2e9bc833755
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-01 12:39:26 +0200

    mstone 1: add data folder for storage, add index, show and destroy CRUD operation

---
+++ routes/posts.js
@@ -0,0 +1,72 @@
+
+const express = require('express');
+const router = express.Router();
+
+// Importing the posts data from db.js
+const posts = require('../data/db');
+
+//Index
+router.get('/', (req, res)=>{
+    // res.send('List of posts');
+    res.json(posts)
+})
+
+// Show
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id) ;
+
+    const post = posts.find( post => post.id === id)
+
+    if(!post){
+        res.status(404)
+        return res.json({
+            error: 'True',
+            message:`The post with ID: ${id} is not present`
+        })
+    }
+
+    res.json(post)
+})
+
+//Store
+router.post('/', (req, res) => {
+    res.send('Post aggiunti alla lista')
+})
+
+//Update
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Il post con ID: ${id} è stato aggiornato`)
+})
+
+//Modify
+router.patch('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Il post con ID: ${id} è stato modificato`)
+})
+
+//Destroy
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id) ;
+
+    console.log(posts);
+    
+
+    const post = posts.find( post => post.id === id)
+
+    if(!post){
+        res.status(404)
+        return res.json({
+            error: 'True',
+            message:`The post with ID: ${id} is not present`
+        })
+    }
+
+    posts.splice(posts.indexOf(post), 1)
+    console.log(posts);
+    
+    res.sendStatus(204)
+
+})
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const post_router = require('./routers/posts.js')
+const post_router = require('./routes/posts.js')
 
 // Importing the posts data from db.js
-const posts = require('./db.js')
+const posts = require('./data/db.js')
 
 app.use('/api/posts', post_router)
 
