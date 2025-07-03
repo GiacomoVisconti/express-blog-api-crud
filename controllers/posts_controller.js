6442824592fd8aee7f1e12b2138a19eb8d9494fe
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-03 11:32:47 +0200

    add comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,12 +7,12 @@ function index(req, res) {
 
     let filtered_posts = posts
 
-    //filtering posts from tags 
-
+    //filtering posts from tags, IF is present
     if(req.query.tag){
         filtered_posts = posts.filter(element => element.tags.includes(req.query.tag))
     }
     
+    //Control the filtered post
     if(filtered_posts.length === 0){
         return res.status(404).json({
             error: 'True',
@@ -33,8 +33,10 @@ function index(req, res) {
 function show(req, res) {
     const id = parseInt(req.params.id) ;
 
+    //finding the post with the specified ID
     const post = posts.find( post => post.id === id)
 
+    //If the post is not found, return a 404 error
     if(!post){
         res.status(404)
         return res.json({
@@ -52,9 +54,10 @@ function destroy(req, res) {
 
     console.log(posts);
     
-
+    //finding the post with the specified ID
     const post = posts.find( post => post.id === id)
 
+    //If the post is not found, return a 404 error
     if(!post){
         res.status(404)
         return res.json({
@@ -74,8 +77,10 @@ function store(req, res) {
     
     console.log(req.body);
 
+    //setting the ID of the new post taking the last ID of the posts array and adding 1
     const new_id = posts[posts.length - 1].id + 1
     
+    //Setting the new post object with the data from the request body
     const new_object = {
         id: new_id,
         title: req.body.title,
@@ -85,9 +90,10 @@ function store(req, res) {
 
     }
     
-
+    //Pushing the new post object into the posts array
     posts.push(new_object)
 
+    //Returning a 201 status code and a success message
     res.status(201).send('Elemento aggiunto correttamente')
 
     console.log(new_object, posts);
@@ -102,9 +108,10 @@ function update(req, res) {
 
     console.log(posts);
     
-
+    //finding the post with the specified ID
     const post = posts.find( post => post.id === id)
 
+    //If the post is not found, return a 404 error
     if(!post){
         res.status(404)
         return res.json({
@@ -113,7 +120,7 @@ function update(req, res) {
         })
     }
 
-
+    //If the post is found, update the post with the data from the request body
     post.title = req.body.title
     post.content = req.body.content
     post.image = req.body.image
@@ -130,4 +137,5 @@ function modify(req, res) {
     res.send(`Il post con ID: ${id} è stato modificato`)
 }
 
+// Exporting the functions to be used in the routes
 module.exports = {index, show, destroy, store, update, modify}
--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const post_router = require('./routes/posts.js')
 // Importing the posts data from db.js
 const posts = require('./data/db.js')
 
+//Set Body Parser
 app.use(express.json())
+
+// Set the router for the posts API
 app.use('/api/posts', post_router)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
+
 
 app.get('/', (req, res) => {
     // res.json(posts)
